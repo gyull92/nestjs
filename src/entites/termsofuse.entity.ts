9489f68016a347e1d098e8e544f37dfb97d59bb9
gyull92 <wjsrbfuf@naver.com>
2023-08-09 20:30:45 +0900

    docs: swagger description add

--- src/entites/termsofuse.entity.ts
+++ src/entites/termsofuse.entity.ts
@@ -1,5 +1,6 @@
 import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "./user.entity";
+import { ApiPropertyOptional } from "@nestjs/swagger";
 
 @Entity()
 export class TermsOfUse extends BaseEntity {
@@ -7,12 +8,15 @@ export class TermsOfUse extends BaseEntity {
     @PrimaryGeneratedColumn({ type: 'int' })
     id: number;
 
+    @ApiPropertyOptional({description: '서비스 이용약관'})
     @Column({ type: 'boolean', default: '1' })
     terms: boolean;
 
+    @ApiPropertyOptional({description: '위치서비스 이용약관'})
     @Column({ type: 'boolean', default: '1' })
     location: boolean;
 
+    @ApiPropertyOptional({description: '운영정책 동의'})
     @Column({ type: 'boolean', default: '1' })
     accept: boolean;
 
